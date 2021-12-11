0e1723111d048ee3999003ea288b1e9c79c6efe4
WOo0W <24245520+WOo0W@users.noreply.github.com>
2021-12-11 23:55:55 +0800

    create template

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>WWW</div>
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
